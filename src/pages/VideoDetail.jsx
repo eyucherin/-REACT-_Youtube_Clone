ee135934a6ebc262a64207ec259aebbef3299ab9
eyucherin <yu24c@mtholyoke.edu>
2023-10-14 20:15:27 -0400

    fetching mock data from videos route

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 export default function VideoDetail() {
+
     const {videoId} = useParams();
   return (
     <div>Video Detail {videoId}</div>
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,23 +1,52 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Videos() {
-    const [text, setText] = useState("");
-    const navigate = useNavigate();
-    const handleChange = (event) => {
-        setText(event.target.value);
+  const [info, setInfo] = useState([]);
+  const [text, setText] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let fetchData = async () => {
+      let response = await fetch(`data/mock_data1.json`);
+      let data = await response.json();
+      await setInfo(data.items);
     };
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        navigate(`/videos/${text}`);
-        setText("");
-    }
+    fetchData();
+  }, []);
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate(`/videos/${text}`);
+    setText("");
+  };
+
   return (
     <div>
       Videos
       <form>
-        <input type = "text" placeholder="video id: " value = {text} onChange = {handleChange}></input>
-        <button type = "submit" onClick = {handleSubmit}>Submit</button>
+        <input
+          type="text"
+          placeholder="video id: "
+          value={text}
+          onChange={handleChange}
+        ></input>
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+        <ul>
+          {info.map((item) => {
+            console.log(item?.id?.videoId)
+            if (item?.id?.videoId){
+              return <li key={item?.id?.videoId}>{item?.snippet?.title}</li>;
+            }
+            return <li key={item?.id?.channelId}>{item?.snippet?.title}</li>;
+          })}
+        </ul>
       </form>
     </div>
   );
